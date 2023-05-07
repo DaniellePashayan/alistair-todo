902f41765e735f027d808c2a37863e03cf70f8ba
DaniellePashayan <daniellepashayan@gmail.com>
2023-05-07 01:40:48 -0400

    made navbar dynamic to page size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Features from "./pages/Features";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/About" element={<HomePage />} />
-        <Route path="/Features" element={<HomePage />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Features" element={<Features />} />
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </>
   );
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,14 @@
 import {
+  Box,
   Button,
   Flex,
   Grid,
   GridItem,
   HStack,
+  Hide,
   Image,
   Link,
+  Show,
   Spacer,
   Text,
 } from "@chakra-ui/react";
@@ -22,31 +25,48 @@ const NavBar = () => {
     >
       <HStack>
         <Image boxSize={"75px"} src={Logo} />
-        <Text as="b" fontSize={"3xl"} color="sgray.100">
+        <Link as="b" fontSize={"3xl"} color="sgray.100">
           {" "}
           Alistair | To Do
-        </Text>
+        </Link>
       </HStack>
       <Spacer />
-      <Grid
-        templateColumns={"repeat(3,1fr)"}
-        alignItems={"center"}
-        fontSize={"xl"}
-        gap={6}
-        mr={6}
-      >
-        <GridItem>
-          <Link href="/About" width={"100px"} color="sgray.100">
-            About
+      <Show below="md">
+        <Flex mr={6} alignContent={"center"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="40"
+            height="40"
+            viewBox="0 0 24 24"
+            fill="gray.900"
+          >
+            <path d="M 3 5 A 1.0001 1.0001 0 1 0 3 7 L 21 7 A 1.0001 1.0001 0 1 0 21 5 L 3 5 z M 3 11 A 1.0001 1.0001 0 1 0 3 13 L 21 13 A 1.0001 1.0001 0 1 0 21 11 L 3 11 z M 3 17 A 1.0001 1.0001 0 1 0 3 19 L 21 19 A 1.0001 1.0001 0 1 0 21 17 L 3 17 z" />
+          </svg>
+        </Flex>
+      </Show>
+      <Hide below="md">
+        <Grid
+          templateColumns={"repeat(3,1fr)"}
+          alignItems={"center"}
+          fontSize={"xl"}
+          gap={6}
+          mr={6}
+        >
+          <GridItem>
+            <Link href="/About" width={"100px"} color="sgray.100">
+              About
+            </Link>
+          </GridItem>
+          <Link href="/Features" width={"100px"} color="sgray.100">
+            Features
           </Link>
-        </GridItem>
-        <Link href="/Features" width={"100px"} color="sgray.100">
-          Features
-        </Link>
-        <Button fontSize={"xl"} bg="pblue.500" color="sgray.100">
-          Log In
-        </Button>
-      </Grid>
+          <Button fontSize={"xl"} bg="pblue.500" color="sgray.100">
+            Log In
+          </Button>
+        </Grid>
+      </Hide>
     </Flex>
   );
 };
